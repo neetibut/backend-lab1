98364805814b79951dcb4966db6fa1577cd4cd3d
neetibut <neetibut@gmail.com>
2025-04-09 11:00:07 +0700

    cleared code for starter

--- client.js
+++ client.js
@@ -1,12 +1,8 @@
 // Import Axios, a promise-based HTTP client that allows us to make requests to external servers or APIs
-const axios = require("axios");
 
 // Create a data object representing the title and content of the new post
 // This object will be sent to the server in the POST request body
-const data = {
-  title: "New Post Title",
-  content: "New Post Content",
-};
+const data = {};
 
 // Make a POST request to the server at 'http://localhost:3000/posts'
 // This sends the 'data' object as the body of the request to create a new post
@@ -14,11 +10,4 @@ const data = {
 // We log the response from the server, which usually includes the created post
 // If there's an error during the request (e.g., server is down or input is invalid), this block will run
 // We log the error to help with debugging
-axios
-  .post("http://localhost:3000/posts", data)
-  .then((response) => {
-    console.log("Post created successfully:", response.data);
-  })
-  .catch((error) => {
-    console.error("Error creating post:", error);
-  });
+axios;
--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 // Import the Express module, which helps us build a web server
 
-const express = require("express");
 // Create an instance of an Express app
-const app = express();
 
 // Define the port number where the server will listen for requests
-const port = 3000;
 
 // Use Express's built-in middleware to parse incoming JSON data in request bodies
-app.use(express.json());
 
 // Create a temporary array to store posts in memory (this simulates a database)
-let posts = [];
 
 // Set up a route to handle POST requests made to "/posts"
 // This route will receive data from the client and save it as a new post
@@ -19,22 +14,8 @@ let posts = [];
 // Create a new post object with a unique id and the received title and content
 // Add the new post to the posts array
 // Respond with a 201 status code (Created) and send back the new post as JSON
-app.post("/posts", (req, res) => {
-  const { title, content } = req.body;
-
-  const newPost = {
-    id: posts.length + 1,
-    title: title,
-    content: content,
-  };
-
-  posts.push(newPost);
-
-  res.status(201).json(newPost);
-});
+app.post("/posts", (req, res) => {});
 
 // Start the server and have it listen on the specified port
 // Log a message to let us know the server is running
-app.listen(port, () => {
-  console.log("Server running at http://localhost:3000");
-});
+app.listen(port, () => {});
