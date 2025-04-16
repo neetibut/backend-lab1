e21954d5765d923e5fc3cb08e2d1030ef979b3af
neetibut <neetibut@gmail.com>
2025-04-16 11:36:40 +0700

    files: comments and missing codes for starter

--- client.js
+++ client.js
@@ -1,29 +1,26 @@
 // Import Axios, a promise-based HTTP client that allows us to make requests to external servers or APIs
 // ⬇️ Code here
-const axios = require("axios");
+const axios = require();
 
 // Create a data object representing the title and content of the new post
 // This object will be sent to the server in the POST request body
 // ⬇️ Code here
-const data = {
-  title: "New Post Title",
-  content: "New Post Content",
-};
+const data = {};
 
 // Make a POST request to the server at 'http://localhost:3000/posts'
 // This sends the 'data' object as the body of the request to create a new post
 // ⬇️ Code here
 axios
-  .post("http://localhost:3000/posts", data)
-  .then((response) => {
+  .post()
+  .then(() => {
     // If the request is successful, this block will run
     // We log the response from the server, which usually includes the created post
     // ⬇️ Code here
-    console.log("Post created successfully:", response.data);
+    console.log();
   })
-  .catch((error) => {
+  .catch(() => {
     // If there's an error during the request (e.g., server is down or input is invalid), this block will run
     // We log the error to help with debugging
     // ⬇️ Code here
-    console.error("Error creating post:", error);
+    console.error();
   });
--- server.js
+++ server.js
@@ -1,41 +1,36 @@
 // 🎯 Import the Express module, which helps us build a web server
 // ⬇️ Code here
-const express = require("express");
+const express = require();
 // Create an instance of an Express app
 // ⬇️ Code here
-const app = express();
+const app = "?";
 // Define the port number where the server will listen for requests
 // ⬇️ Code here
-const port = 3000;
+const port = "?";
 // Use Express's built-in middleware to parse incoming JSON data in request bodies
 // ⬇️ Code here
-app.use(express.json());
+app.use();
 // Create a temporary array to store posts in memory (this simulates a database)
 // ⬇️ Code here
-let posts = [];
+let posts = "?";
 
 // Set up a route to handle POST requests made to "/posts"
 // This route will receive data from the client and save it as a new post
 
 app.post("/posts", (req, res) => {
   // ⬇️ Code here
-  const { title, content } = req.body;
+  const {} = req.body;
   // Example: Save the post to a data store
   // Create a new post object with a unique id and the received title and content
-  const newPost = {
-    id: posts.length + 1,
-    title: title,
-    content: content,
-  };
+  const newPost = {};
   // Add the new post to the posts array
-  posts.push(newPost);
+  posts.push();
   // Respond with a 201 status code (Created) and send back the new post as JSON
-  res.status(201).json(newPost);
+  res.status().json();
 });
 
 // Start the server and have it listen on the specified port
 // Log a message to let us know the server is running
 app.listen(port, () => {
   // ⬇️ Code here
-  console.log(`Server runnning at http://localhost:${port}`);
 });
